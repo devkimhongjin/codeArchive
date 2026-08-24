29aad74801d51cf83aa1962426ce44d7b7cca3c5
devkimhongjin <dev.kimhongjin@gmail.com>
2026-08-24 15:15:24 +0900

    fix(extension): import renamed popup component

--- apps/extension/src/popup.tsx
+++ apps/extension/src/popup.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Popup } from "./Popup";
+import { Popup } from "./PopupView";
 
 const root = document.getElementById("root");
 
