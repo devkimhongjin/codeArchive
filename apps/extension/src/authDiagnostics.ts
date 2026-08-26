0acab26d073137847583d6360a7c7e57411d1ed8
devkimhongjin <dev.kimhongjin@gmail.com>
2026-08-26 17:56:09 +0900

    feat(extension): narrow login-start fetch diagnostics

--- apps/extension/src/authDiagnostics.ts
+++ apps/extension/src/authDiagnostics.ts
@@ -2,6 +2,8 @@ export type AuthLoginFailureStage =
   | "login_start"
   | "login_start_host_access"
   | "login_start_fetch"
+  | "login_start_fetch_origin"
+  | "login_start_fetch_route"
   | "login_start_http"
   | "login_start_json"
   | "login_start_envelope"
