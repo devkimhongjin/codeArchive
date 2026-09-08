fada7a99f2ce4a170c90a6e2bb7dc10f0dce56d0
devkimhongjin <dev.kimhongjin@gmail.com>
2026-09-08 09:24:21 +0900

    test(api): import profile assertions

--- apps/api/src/test/java/com/codearchive/api/automation/DurableAutomationProfileServiceTest.java
+++ apps/api/src/test/java/com/codearchive/api/automation/DurableAutomationProfileServiceTest.java
@@ -1,6 +1,7 @@
 package com.codearchive.api.automation;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
