62e57ed2562bfba75b16e0acac5afa052bb405a0
devkimhongjin <dev.kimhongjin@gmail.com>
2026-09-15 12:22:55 +0900

    fix(web): render dashboard without api readiness gate

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { ApiReadinessGate } from "./ApiReadinessGate";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ApiReadinessGate><App /></ApiReadinessGate>
+    <App />
   </StrictMode>,
 );
